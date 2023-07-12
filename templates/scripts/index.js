93534692712ab61b90d26c9374550fddc4cc7627
mgodunow <mikhailgodunow@gmail.com>
2023-07-12 11:21:59 +0300

    Finished login and registration form

--- templates/scripts/index.js
+++ templates/scripts/index.js
@@ -1,8 +1,10 @@
+const modalWrapper = document.querySelector('.modal-wrapper');
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 const btnPopup = document.querySelector('.login-button');
 const iconClose = document.querySelector('.icon-close');
+const overlay = document.querySelector('.overlay');
 
 registerLink.addEventListener('click', ()=> {
     wrapper.classList.add('active');
@@ -14,8 +16,15 @@ loginLink.addEventListener('click', ()=> {
 
 btnPopup.addEventListener('click', ()=> {
     wrapper.classList.add('active-popup');
+    modalWrapper.classList.add('active-popup')
 })
 
 iconClose.addEventListener('click', ()=> {
     wrapper.classList.remove('active-popup');
+    modalWrapper.classList.remove('active-popup')
+})
+
+overlay.addEventListener('click', ()=> {
+    wrapper.classList.remove('active-popup')
+    modalWrapper.classList.remove('active-popup')
 })
